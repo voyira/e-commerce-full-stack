81c92161e991a0bbcf06a6c131ff59e51f1b8820
birgulkurtcu <157323413+birgulkurtcu@users.noreply.github.com>
2025-04-20 15:52:05 +0300

    CartDrawer.jsx added

---
+++ frontend/src/components/Layout/CartDrawer.jsx
@@ -0,0 +1,32 @@
+import {IoMdClose} from "react-icons/io";
+import CartContents from "../Cart/CartContents.jsx";
+
+const CartDrawer = ({drawerOpen ,toggleCartDrawer}) => {
+    return (
+        <div className={`fixed top-0 right-0 w-3/4 sm:w-1/2 md:w-[30rem] h-full bg-white transform transition-transform duration-300 flex flex-col z-50 ${
+            drawerOpen ? "translate-x-0" : "translate-x-full"
+        }`}
+        >
+            {/*{Close Button}*/}
+            <div className={"flex justify-end p-4"}>
+                <button>
+                    <IoMdClose  className={"h-6 w-6 text-gray-600"}/>
+                </button>
+            </div>
+            {/*{Cart contents with scrollable area}*/}
+            <div className={"flex-grow p-4 overflow-y-auto"}>
+                <h2 className={"text-xl font-semibold mb-4"}>Your Cart</h2>
+                <CartContents/>
+            </div>
+            {/*{Checkout button fixed at the bottom}*/}
+            <div className={"p-4 bg-white sticky bottom-0"}>
+                <button className={"w-full bg-black text-white py-3 rounded-lg font-semibold hover:bg-gray-800 transition"}>Checkout</button>
+                <p className={"text-sm tracking-tighter text-gray-500 mt-2 text-center"}>
+                    Shipping, taxes and discount codes calculated at checkout.
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default CartDrawer;
