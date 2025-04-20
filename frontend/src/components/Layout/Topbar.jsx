3e92cc0302be43041b4762a6f575c699173fe244
birgulkurtcu <157323413+birgulkurtcu@users.noreply.github.com>
2025-04-20 14:30:46 +0300

    topbar updated

--- frontend/src/components/Layout/Topbar.jsx
+++ frontend/src/components/Layout/Topbar.jsx
@@ -4,9 +4,9 @@ import {RiTwitterXLine} from "react-icons/ri";
 
 const Topbar = () => {
     return (
-        <div className={"bg-[#ea2e0e] text-white"}>
-            <div className={"container mx-auto"}>
-                <div className={"flex items-center space-x-4"}>
+        <div className={"bg-primary-color text-white"}>
+            <div className={"container mx-auto flex justify-between items-center py-3 px-4"}>
+                <div className={"hidden md:flex items-center space-x-4"}>
                     <a href={"#"} className={"hover:text-gray-300"}>
                         <TbBrandMeta className={"h-5 w-5 "}/>
                     </a>
@@ -17,7 +17,7 @@ const Topbar = () => {
                         <RiTwitterXLine className={"h-5 w-5 "}/>
                     </a>
                 </div>
-                <div className={"text-sm text-center"}>
+                <div className={"text-sm text-center flex-grow"}>
                     <span>We ship worldwide - Fast and reliable shipping!</span>
                 </div>
                 <div className={"text-sm"}>
