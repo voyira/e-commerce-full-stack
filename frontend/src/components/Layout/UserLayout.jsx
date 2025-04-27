757bf98d57d9b61d0df38f9b86e875a7dcc1af9d
birgulkurtcu <157323413+birgulkurtcu@users.noreply.github.com>
2025-04-27 13:28:15 +0300

    UserLayout.jsx updated

--- frontend/src/components/Layout/UserLayout.jsx
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
 import Header from "../Common/Header.jsx";
+import Footer from "../Common/Footer.jsx";
+import {Outlet} from "react-router-dom";
 
 const UserLayout = () => {
     return (
         <div>
             <Header/>
+            <main>
+                <Outlet/>
+            </main>
+            <Footer/>
         </div>
     );
 };
