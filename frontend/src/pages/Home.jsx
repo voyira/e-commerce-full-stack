35ceeaaf9fc904acabe59d12658b7c0a2cf56904
birgulkurtcu <157323413+birgulkurtcu@users.noreply.github.com>
2025-06-24 01:06:29 +0300

    home.jsx updated

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Hero from "../components/Layout/Hero.jsx";
 import GenderCollectionSection from "../components/Products/GenderCollectionSection.jsx";
 import NewArrivals from "../components/Products/NewArrivals.jsx";
+import ProductDetails from "../components/Products/ProductDetails.jsx";
 
 const Home = () => {
     return (
@@ -8,6 +9,9 @@ const Home = () => {
             <Hero/>
             <GenderCollectionSection/>
             <NewArrivals/>
+            {/*bestseller*/}
+            <h2 className={"text-3xl text-center font-bold mb-4"}>Best Seller</h2>
+            <ProductDetails/>
         </div>
     );
 };
