41e4db3c124e62acf035072e9f26e9d459650a4e
birgulkurtcu <157323413+birgulkurtcu@users.noreply.github.com>
2025-06-27 00:52:24 +0300

    ProductDetails.jsx updated

--- frontend/src/components/Products/ProductDetails.jsx
+++ frontend/src/components/Products/ProductDetails.jsx
@@ -1,7 +1,8 @@
 import React, {useEffect, useState} from 'react';
+import {toast} from "sonner";
 
 const selectedProduct = {
-    name: "Stylish JAcket",
+    name: "Stylish Jacket",
     price: 120,
     originalPrice: 150,
     description: " This is stylish Jacket perfect for any occasion.",
@@ -9,42 +10,60 @@ const selectedProduct = {
     material: "Leather",
     sizes: ["S", "M", "L", "XL"],
     colors: ["Red", "Blue"],
-    images: [
-        {
-            url: "https://picsum.photos/500/500?random=1",
-            altText: "Stylish Jacket 1",
-        },
-        {
-            url: "https://picsum.photos/500/500?random=2",
-            altText: "Stylish Jacket 2",
-        },
-
+    images: [{
+        url: "https://picsum.photos/500/500?random=1", altText: "Stylish Jacket 1",
+    }, {
+        url: "https://picsum.photos/500/500?random=2", altText: "Stylish Jacket 2",
+    },
     ]
-
 }
-
 const ProductDetails = () => {
     const [mainImage, setMainImage] = useState("");
+    const [selectedSize, setSelectedSize] = useState("");
+    const [selectedColor, setSelectedColor] = useState("");
+    const [quantity, setQuantity] = useState(1);
+    const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+
     useEffect(() => {
-        if(selectedProduct?.images?.length>0) {
+        if (selectedProduct?.images?.length > 0) {
             setMainImage(selectedProduct.images[0].url);
         }
     }, [selectedProduct]);
+
+    const handleQuantityChange = (action) => {
+        if (action === "plus") setQuantity((prev) => prev + 1);
+        if (action === "minus" && quantity > 1) setQuantity((prev) => prev - 1);
+    }
+    const handleAddToCart = () => {
+        if (!selectedSize || !selectedColor) {
+            toast.error("Please select a size and color before adding to cart.", {
+                duration: 1000,
+            });
+            return;
+        }
+        setIsButtonDisabled(true);
+        setTimeout(() => {
+            toast.success("Product added to cart", {
+                duration: 1000,
+            });
+            setIsButtonDisabled(false);
+        }, 500);
+    }
     return (
         <div className={"p-6"}>
             <div className={"max-w-6xl mx-auto bg-white p-8 rounded-lg"}>
                 <div className={"flex flex-col md:flex-row"}>
                     {/*Left Thumbnails*/}
                     <div className={"hidden md:flex flex-col space-y-4 mr-6 "}>
-                        {selectedProduct.images.map((image, index) => (
-                            <img
-                                key={index}
-                                src={image.url}
-                                alt={image.altText || `Thumbnail ${index}`}
-                                className={"w-20 h-20 object-cover rounded-lg cursor-pointer border"}
-                                onClick={() => setMainImage(image.url)}
-                            />
-                        ))}
+                        {selectedProduct.images.map((image, index) => (<img
+                            key={index}
+                            src={image.url}
+                            alt={image.altText || `Thumbnail ${index}`}
+                            className={`w-20 h-20 object-cover rounded-lg cursor-pointer border ${
+                                mainImage === image.url ? "border-black" : "border-gray-300"
+                            }`}
+                            onClick={() => setMainImage(image.url)}
+                        />))}
                     </div>
                     {/*Main Image*/}
                     <div className={"md:w-1/2"}>
@@ -62,7 +81,10 @@ const ProductDetails = () => {
                                 key={index}
                                 src={image.url}
                                 alt={image.altText || `Thumbnail ${index}`}
-                                className={"w-20 h-20 object-cover rounded-lg cursor-pointer border"}
+                                className={`w-20 h-20 object-cover rounded-lg cursor-pointer border ${
+                                    mainImage === image.url ? "border-black" : "border-gray-300"
+                                }`}
+                                onClick={() => setMainImage(image.url)}
                             />
                         ))}
                     </div>
@@ -83,21 +105,14 @@ const ProductDetails = () => {
                         <div className={"mb-4"}>
                             <p className={"text-gray-700"}>Color:</p>
                             <div className={"flex gap-2 mt-2"}>
-                                {selectedProduct.colors.map((color) => (
-                                    <button
-                                        key={color}
-                                        onClick={() => setSelectedColor(color)}
-                                        className={`w-8 h-8 rounded-full border ${
-                                            selectedColor === color
-                                                ? "border-4 border-black"
-                                                : "border-gray-300"
-                                        }`}
-                                        style={{
-                                            backgroundColor: color.toLocaleLowerCase(),
-                                            filter: "brightness(0.5)",
-                                        }}
-                                    ></button>
-                                ))}
+                                {selectedProduct.colors.map((color) => (<button
+                                    key={color}
+                                    onClick={() => setSelectedColor(color)}
+                                    className={`w-8 h-8 rounded-full border ${selectedColor === color ? "border-4 border-black" : "border-gray-300"}`}
+                                    style={{
+                                        backgroundColor: color.toLocaleLowerCase(), filter: "brightness(0.5)",
+                                    }}
+                                ></button>))}
                             </div>
                         </div>
                         <div className={"mb-4"}>
@@ -107,13 +122,10 @@ const ProductDetails = () => {
                                     <button
                                         key={size}
                                         onClick={() => setSelectedSize(size)}
-                                        className={`px-4 py-2 rounded border ${
-                                            selectedSize === size ? "bg-black text-white" : ""
-                                        }`}
+                                        className={`px-4 py-2 rounded border ${selectedSize === size ? "bg-black text-white" : ""}`}
                                     >
                                         {size}
-                                    </button>
-                                ))}
+                                    </button>))}
                             </div>
                         </div>
                         <div className={"mb-6"}>
@@ -140,8 +152,7 @@ const ProductDetails = () => {
                             className={`bg-black text-white py-2 px-6 rounded w-full mb-4 ${
                                 isButtonDisabled
                                     ? "cursor-not-allowed opacity-50"
-                                    : "hover:bg-gray-900"
-                            }`}
+                                    : "hover:bg-gray-900"}`}
                         >
                             {isButtonDisabled ? "Adding..." : "ADD TO CART"}
                         </button>
@@ -162,13 +173,9 @@ const ProductDetails = () => {
                         </div>
                     </div>
                 </div>
-
             </div>
         </div>
-</div>
-</div>
-)
-    ;
+    )
 };
 
 export default ProductDetails;
