6ca5bd8e2a826488cd009859a0ddbb9066b72468
birgulkurtcu <157323413+birgulkurtcu@users.noreply.github.com>
2025-06-27 00:52:40 +0300

    app.jsx updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import UserLayout from "./components/Layout/UserLayout.jsx";
 import Home from "./pages/Home.jsx";
+import {Toaster} from "sonner";
 
 const App = () => {
     return (
         <BrowserRouter>
+            <Toaster position={"top-right"}/>
             <Routes>
                 <Route path="/" element={<UserLayout/>}>
                     <Route index element={<Home/>}/>
