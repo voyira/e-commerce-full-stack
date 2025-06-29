c865ba7955085d7650dcd737d17882b286c62490
birgulkurtcu <157323413+birgulkurtcu@users.noreply.github.com>
2025-06-29 23:58:24 +0300

    App.jsx updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import UserLayout from "./components/Layout/UserLayout.jsx";
 import Home from "./pages/Home.jsx";
 import {Toaster} from "sonner";
+import Login from "./pages/Login.jsx";
 
 const App = () => {
     return (
@@ -10,6 +11,7 @@ const App = () => {
             <Routes>
                 <Route path="/" element={<UserLayout/>}>
                     <Route index element={<Home/>}/>
+                    <Route path ="login" element={<Login/>}/>
                 </Route>
                 <Route>{/*Admin Layout*/}</Route>
             </Routes>
