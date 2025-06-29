4437e7feaad0af3524b81910219d479a1acc0f5a
birgulkurtcu <157323413+birgulkurtcu@users.noreply.github.com>
2025-06-30 00:46:09 +0300

    App.jsx routes updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import UserLayout from "./components/Layout/UserLayout.jsx";
 import Home from "./pages/Home.jsx";
 import {Toaster} from "sonner";
 import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
+import Profile from "./pages/Profile.jsx";
 
 const App = () => {
     return (
@@ -12,6 +14,8 @@ const App = () => {
                 <Route path="/" element={<UserLayout/>}>
                     <Route index element={<Home/>}/>
                     <Route path ="login" element={<Login/>}/>
+                    <Route path ="register" element={<Register/>}/>
+                    <Route path ="profile" element={<Profile/>}/>
                 </Route>
                 <Route>{/*Admin Layout*/}</Route>
             </Routes>
