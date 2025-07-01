96657f73b811743eefa4e9171d1e4c758cbdf0ba
birgulkurtcu <157323413+birgulkurtcu@users.noreply.github.com>
2025-07-02 00:55:26 +0300

    CartDrawer.jsx updated

--- frontend/src/components/Layout/CartDrawer.jsx
+++ frontend/src/components/Layout/CartDrawer.jsx
@@ -1,7 +1,13 @@
 import {IoMdClose} from "react-icons/io";
 import CartContents from "../Cart/CartContents.jsx";
+import {useNavigate} from "react-router-dom";
 
 const CartDrawer = ({drawerOpen ,toggleCartDrawer}) => {
+    const navigate = useNavigate();
+    const handleCheckout = () => {
+        navigate("/checkout")
+    }
+
     return (
         <div className={`fixed top-0 right-0 w-3/4 sm:w-1/2 md:w-[30rem] h-full bg-white transform transition-transform duration-300 flex flex-col z-50 ${
             drawerOpen ? "translate-x-0" : "translate-x-full"
@@ -20,7 +26,8 @@ const CartDrawer = ({drawerOpen ,toggleCartDrawer}) => {
             </div>
             {/*{Checkout button fixed at the bottom}*/}
             <div className={"p-4 bg-white sticky bottom-0"}>
-                <button className={"w-full bg-black text-white py-3 rounded-lg font-semibold hover:bg-gray-800 transition"}>Checkout</button>
+                <button onClick={handleCheckout}
+                        className={"w-full bg-black text-white py-3 rounded-lg font-semibold hover:bg-gray-800 transition"}>Checkout</button>
                 <p className={"text-sm tracking-tighter text-gray-500 mt-2 text-center"}>
                     Shipping, taxes and discount codes calculated at checkout.
                 </p>
