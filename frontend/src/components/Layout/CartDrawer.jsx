755151dbad40230a05f4a1d020a92b4a4d17bd39
birgulkurtcu <157323413+birgulkurtcu@users.noreply.github.com>
2025-08-04 00:22:11 +0300

    CartDrawer.jsx updated

--- frontend/src/components/Layout/CartDrawer.jsx
+++ frontend/src/components/Layout/CartDrawer.jsx
@@ -5,6 +5,7 @@ import {useNavigate} from "react-router-dom";
 const CartDrawer = ({drawerOpen ,toggleCartDrawer}) => {
     const navigate = useNavigate();
     const handleCheckout = () => {
+        toggleCartDrawer();
         navigate("/checkout")
     }
 
