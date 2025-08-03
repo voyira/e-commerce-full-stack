c23967020d181fe2b5adff712f4cb94f853d3340
birgulkurtcu <157323413+birgulkurtcu@users.noreply.github.com>
2025-08-04 00:21:58 +0300

    MyOrdersPage.jsx updated

--- frontend/src/pages/MyOrdersPage.jsx
+++ frontend/src/pages/MyOrdersPage.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
+import {useNavigate} from "react-router-dom";
 
 const MyOrdersPage = () => {
     const [orders, setOrders] = useState([]);
+    const navigate = useNavigate();
 
     useEffect(() => {
         // simulate fetching orders
@@ -37,6 +39,9 @@ const MyOrdersPage = () => {
             setOrders(mockOrders);
         }, 1000);
     }, []);
+    const handleRowClick = (orderId) => {
+        navigate(`/order/${orderId}`);
+    };
 
     return (
         <div className={"max-w-7xl mx-auto p-4 sm:p-6"}>
@@ -59,6 +64,7 @@ const MyOrdersPage = () => {
                         orders.map((order) => (
                             <tr
                                 key={order._id}
+                                onClick={() => handleRowClick(order._id)}
                                 className={"border-b hover:bg-gray-50 cursor-pointer"}
                             >
                                 <td className={"py-2 px-2 sm:py-4 sm:px-4"}>
