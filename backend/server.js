64e1b7bce6494682025b811b0d61041fc9b20670
birgulkurtcu <157323413+birgulkurtcu@users.noreply.github.com>
2025-08-05 21:57:28 +0300

    server.js added

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+dotenv.config();
+console.log(process.env.PORT);
+
+const PORT = process.env.PORT || 3000;
+
+connectDB();
+
+app.get('/', (req, res) => {
+    res.send("WELCOME TO ROCKSET API!");
+});
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
