1e3847a1f4e15c1332454a9e63c3b17a12ab03ac
birgulkurtcu <157323413+birgulkurtcu@users.noreply.github.com>
2025-08-07 01:33:14 +0300

    server.js updated

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
+const productRoutes = require('./routes/productRoutes');
+const cartRoutes = require('./routes/cartRoutes');
 
 const app = express();
 app.use(express.json());
@@ -21,6 +23,8 @@ app.get('/', (req, res) => {
 
 //API Routes
 app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
