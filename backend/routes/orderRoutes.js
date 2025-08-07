278785f39083caef9d757755f577682d5a279801
birgulkurtcu <157323413+birgulkurtcu@users.noreply.github.com>
2025-08-08 02:21:41 +0300

    orderRoutes.js added

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const Order = require("../models/Order");
+const { protect } = require("../middleware/authMiddleware");
+const router = express.Router();
+//@route GET /api/orders/my-orders
+// @desc Get logged-in user's orders
+// @access Private
+router.get("/my-orders", protect, async (req, res) => {
+    try {
+        // Find order for the authenticated user
+        const orders = await Order.find({ user: req.user._id }).sort({
+            createdAt: -1,
+        });//sort by most recent orders
+        res.json(orders);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server error" });
+    }
+});
+//@route GET /api/orders/:id
+// @desc Get order details by ID
+// @access Private
+router.get("/:id", protect, async (req, res) => {
+    try {
+        const order = await Order.findById(req.params.id)
+            .populate("user", "name email");
+        if (!order) {
+            return res.status(404).json({ message: "Order not found" });
+        }
+        res.json(order);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server error" });
+    }
+});
+module.exports = router;
