6e050d117f0659086ed4f5094baceb201dfd2c71
birgulkurtcu <157323413+birgulkurtcu@users.noreply.github.com>
2025-08-08 03:20:46 +0300

    server.js updated

--- backend/server.js
+++ backend/server.js
@@ -5,27 +5,39 @@ const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
 const cartRoutes = require('./routes/cartRoutes');
+const checkoutRoutes = require('./routes/checkoutRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+const uploadRoutes = require('./routes/uploadRoutes');
+const subscribeRoute = require('./routes/subscribeRoute');
+const adminRoutes = require('./routes/adminRoutes');
+const productAdminRoutes = require('./routes/productAdminRoutes');
+const adminOrderRoutes = require('./routes/adminOrderRoutes');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-
 dotenv.config();
 console.log(process.env.PORT);
-
 const PORT = process.env.PORT || 3000;
-
+//Connect to MongoDB
 connectDB();
 
 app.get('/', (req, res) => {
     res.send("WELCOME TO ROCKSET API!");
 });
-
 //API Routes
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
+app.use("/api/checkout", checkoutRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
+app.use("/api/subscribe", subscribeRoute);
 
+//Admin Routes
+app.use("/api/admin/users", adminRoutes);
+app.use("/api/admin/products", productAdminRoutes);
+app.use("/api/admin/orders", adminOrderRoutes);
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 })
