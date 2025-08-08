99ab84f2f9705b4270ef5a607fec6e938c45a9c7
birgulkurtcu <157323413+birgulkurtcu@users.noreply.github.com>
2025-08-08 03:19:34 +0300

    adminOrderRoutes.js, productAdminRoutes.js added

---
+++ backend/routes/productAdminRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const Product = require("../models/Product");
+const {protect, admin} = require("../middleware/authMiddleware");
+const router = express.Router();
+// @route GET /api/admin/products
+// @desc Get all products (admin only)
+// @access Private/Admin
+router.get("/", protect, admin, async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.json(products);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: "Server error"});
+    }
+});
+module.exports = router;
