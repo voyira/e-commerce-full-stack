2d7aba57e766fe852d97e7f1dfce2c0f7b048357
birgulkurtcu <157323413+birgulkurtcu@users.noreply.github.com>
2025-08-17 12:34:58 +0300

    checkoutSlice.js added

---
+++ frontend/src/redux/slices/checkoutSlice.js
@@ -0,0 +1,46 @@
+import {createSlice, createAsyncThunk} from "@reduxjs/toolkit";
+import axios from "../../utils/axios";
+
+//Async thunk to create a checkout session
+export const createCheckout = createAsyncThunk(
+    "checkout/createCheckout",
+    async (checkoutData, {rejectWithValue}) => {
+        try {
+            const response = await axios.post(`$import.meta.VITE_BACKEND_URL}/api/checkout`,
+                checkoutData, {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+                    },
+                });
+            return response.data;
+        } catch (error) {
+            return rejectWithValue(error.response.data);
+        }
+    }
+);
+const checkoutSlice = createSlice({
+    name: "checkout",
+    initialState: {
+        checkout: null,
+        loading: null,
+        error: null,
+    },
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(createCheckout.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+            })
+            .addCase(createCheckout.fulfilled, (state, action) => {
+                state.loading = false;
+                state.checkout = action.payload;
+            })
+            .addCase(createCheckout.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload.message;
+            });
+    },
+});
+
+export default checkoutSlice.reducer;
