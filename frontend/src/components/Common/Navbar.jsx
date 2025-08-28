760ca3d8249fcf4a18921187cdb8a2c80f078e86
birgulkurtcu <157323413+birgulkurtcu@users.noreply.github.com>
2025-08-28 22:42:02 +0300

    Navbar.jsx updated

--- frontend/src/components/Common/Navbar.jsx
+++ frontend/src/components/Common/Navbar.jsx
@@ -4,10 +4,14 @@ import {HiOutlineUser, HiOutlineShoppingBag, HiBars3BottomRight} from "react-ico
 import SearchBar from "./SearchBar.jsx";
 import CartDrawer from "../Layout/CartDrawer.jsx";
 import {IoMdClose} from "react-icons/io";
+import {useSelector} from "react-redux";
 
 const Navbar = () => {
     const [drawerOpen, setDrawerOpen] = useState(false);
     const [navDrawerOpen, setNavDrawerOpen] = useState(false);
+    const {cart} = useSelector((state) => state.cart);
+    const cartItemCount = cart?.products?.reduce((total, product) => total + product.quantity, 0)
+        || 0;
 
     const toggleNavDrawer = () => {
         setNavDrawerOpen(!navDrawerOpen);
@@ -49,10 +53,14 @@ const Navbar = () => {
                     <button onClick={toggleCartDrawer}
                             className={"relative hover:text-black"}>
                         <HiOutlineShoppingBag className={"h-6 w-6 text-gray-700"}/>
-                        <span
-                            className={"absolute -top-1 bg-primary-color text-white text-xs rounded-full px-1 py-0.5"}
-                        >4
-                    </span>
+                        {cartItemCount > 0 && (<span
+                                className={"absolute -top-1 bg-primary-color text-white text-xs rounded-full px-1 py-0.5"}
+                            >
+                                {cartItemCount}
+                           </span>
+                        )}
+
+
                     </button>
                     <div className={"overflow-hidden"}>
                         <SearchBar/>
@@ -64,7 +72,8 @@ const Navbar = () => {
             </nav>
             <CartDrawer drawerOpen={drawerOpen} toggleCartDrawer={toggleCartDrawer}/>
             {/*{Mobile Navigation}*/}
-            <div className={`fixed top-0 left-0 w-3/4 sm:w-1/2 md:w-1/3 h-full bg-white shadow-lg transform transition-transform duration-300 z-50 ${navDrawerOpen ? "translate-x-0" : "-translate-x-full"}`}>
+            <div
+                className={`fixed top-0 left-0 w-3/4 sm:w-1/2 md:w-1/3 h-full bg-white shadow-lg transform transition-transform duration-300 z-50 ${navDrawerOpen ? "translate-x-0" : "-translate-x-full"}`}>
                 <div className={"flex justify-end p-4"}>
                     <button onClick={toggleNavDrawer}>
                         <IoMdClose className={"h-6 w-6 text-gray-600"}/>
