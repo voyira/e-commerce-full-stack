de5e76e19edea285ab597752f8008dc9704c4fc8
birgulkurtcu <157323413+birgulkurtcu@users.noreply.github.com>
2025-08-29 03:16:44 +0300

    OrderConfirmationPage.jsx updated

--- frontend/src/pages/OrderConfirmationPage.jsx
+++ frontend/src/pages/OrderConfirmationPage.jsx
@@ -1,33 +1,23 @@
-const checkout = {
-    _id: "123",
-    createdAt: new Date(),
-    checkoutItems: [
-        {
-            productId: "1",
-            name: "Jacket",
-            color: "black",
-            size: "M",
-            price: "120",
-            quantity: 1,
-            image: "https://picsum.photos/150?random=1",
-        },
-        {
-            productId: "2",
-            name: "T-shirt",
-            color: "black",
-            size: "M",
-            price: "870",
-            quantity: 2,
-            image: "https://picsum.photos/150?random=1",
-        }
-    ],
-    shippingAddress: {
-        address: "123 Street",
-        city: "New York",
-        country: "USA",
-    }
-}
+import {useDispatch, useSelector} from "react-redux";
+import {useNavigate} from "react-router-dom";
+import {useEffect} from "react";
+import {clearCart} from "../redux/slices/cartSlice.js";
+
 const OrderConfirmationPage = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const {checkout} = useSelector((state) => state.checkout);
+
+    //Clear the cart when the order is confirmed
+    useEffect(() => {
+        if (checkout && checkout._id) {
+            dispatch(clearCart());
+            localStorage.removeItem("cart");
+        } else {
+            navigate("/my-orders");
+        }
+    }, [checkout, dispatch, navigate]);
+
     const calculateEstimatedDelivery = (createdAt) => {
         const orderDate = new Date(createdAt)
         orderDate.setDate(orderDate.getDate() + 10) // Add 10 days to the order date
