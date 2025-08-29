5e6b3f9fcab77628c933a4a3e590a566799212ff
birgulkurtcu <157323413+birgulkurtcu@users.noreply.github.com>
2025-08-29 23:56:31 +0300

    adminOrderRoutes.js updated

--- backend/routes/adminOrderRoutes.js
+++ backend/routes/adminOrderRoutes.js
@@ -20,7 +20,7 @@ router.get('/', protect, admin, async (req, res) => {
 // @access Private/Admin
 router.put('/:id', protect, admin, async (req, res) => {
         try {
-            const order = await Order.findById(req.params.id)
+            const order = await Order.findById(req.params.id).populate("user", "name")
             if (order) {
                 order.status = req.body.status || order.status;
                 order.isDelivered = req.body.status === 'Delivered' ? true : order.isDelivered;
