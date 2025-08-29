899d0966673d3f2e12098108a1cbe752cee72d04
birgulkurtcu <157323413+birgulkurtcu@users.noreply.github.com>
2025-08-29 03:17:02 +0300

    OrderDetailsPage.jsx updated

--- frontend/src/pages/OrderDetailsPage.jsx
+++ frontend/src/pages/OrderDetailsPage.jsx
@@ -1,42 +1,24 @@
 import {Link, useParams} from "react-router-dom";
-import {useEffect, useState} from "react";
+import {useEffect} from "react";
+import {fetchOrderDetails} from "../redux/slices/orderSlice.js";
+import {useDispatch, useSelector} from "react-redux";
 
 const OrderDetailsPage = () => {
     const {id} = useParams();
-    const [orderDetails, setOrderDetails] = useState(null);
+    const dispatch = useDispatch();
+
+    const { orderDetails, loading, error } = useSelector((state) => state.orders);
 
     useEffect(() => {
-        const mockOrderDetails = {
-            _id: id,
-            createdAt: new Date(),
-            isPaid: true,
-            isDelivered: false,
-            paymentMethod: "PayPal",
-            shippingMethod: "Standard",
-            shippingAddress: {city: "New York", country: "USA"},
-            orderItems: [
-                {
-                    productId: "1",
-                    name: "Jacket",
-                    color: "black",
-                    size: "M",
-                    price: "120",
-                    quantity: 1,
-                    image: "https://picsum.photos/150?random=1",
-                },
-                {
-                    productId: "2",
-                    name: "T-shirt",
-                    color: "black",
-                    size: "M",
-                    price: "870",
-                    quantity: 2,
-                    image: "https://picsum.photos/150?random=1",
-                }
-            ]
-        }
-        setOrderDetails(mockOrderDetails);
-    }, [id]);
+        dispatch(fetchOrderDetails(id));
+    }, [dispatch, id]);
+
+    if (loading) {
+        return <p>Loading ...</p>
+    }
+    if (error) {
+        return <p>Error : {error} ...</p>
+    }
 
     return (
         <div className={"max-w-7xl mx-auto p-4 sm:p-6"}>
