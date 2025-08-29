ace1c461887d8d241a1eab4bf860457692836372
birgulkurtcu <157323413+birgulkurtcu@users.noreply.github.com>
2025-08-30 00:01:39 +0300

    UserManagement.jsx updated

--- frontend/src/components/Admin/UserManagement.jsx
+++ frontend/src/components/Admin/UserManagement.jsx
@@ -1,14 +1,26 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {useNavigate} from "react-router-dom";
+import {addUser, deleteUser, fetchUsers, updateUser} from "../../redux/slices/adminSlice.js";
 
 const UserManagement = () => {
-    const users = [
-        {
-            _id: 1,
-            name: "John Doe",
-            email: "john@example.com",
-            role: "admin",
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const {user} = useSelector((state) => state.auth);
+    const {users, loading, error} = useSelector((state) => state.admin);
+
+    useEffect(() => {
+        if (user && user.role !== "admin") {
+            navigate("/");
         }
-    ]
+    }, [user, navigate]);
+
+    useEffect(() => {
+        if (user && user.role === "admin") {
+            dispatch(fetchUsers());
+        }
+    }, [dispatch , user]);
+
     const [formData, setFormData] = useState({
         name: "",
         email: "",
@@ -23,6 +35,7 @@ const UserManagement = () => {
     };
     const handleSubmit = (e) => {
         e.preventDefault();
+        dispatch(addUser(formData));
         //reset the form after submission
         setFormData({
             name: "",
@@ -32,17 +45,19 @@ const UserManagement = () => {
         })
     }
     const handleRoleChange = (userId, newRole) => {
-        console.log({id: userId, role: newRole});
+        dispatch(updateUser({id: userId, role: newRole}))
     }
     const handleDeleteUser = (userId) => {
         if (window.confirm("Are you sure you want to delete this user?")) {
-            console.log("deleting user with ID", userId);
+            dispatch(deleteUser(userId))
         }
     }
     return (
         <div className={"max-w-7xl mx-auto p-6"}>
             <h2 className={"text-2xl font-bold mb-6"}>User Management</h2>
-            {/* add the new user form */}
+            {loading && <p>Loading ...</p>}
+            {error && <p>Error : {error}</p>}
+            {/* Add the new user form */}
             <div className={"p-6 rounded-lg mb-6"}>
                 <h3 className={"text-lg font-bold mb-6"}>Add New User</h3>
                 <form onSubmit={handleSubmit}>
