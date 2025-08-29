f9ea04e5ee20a4168bf92fe5baba13293a9101f3
birgulkurtcu <157323413+birgulkurtcu@users.noreply.github.com>
2025-08-30 00:01:11 +0300

    Profile.jsx updated

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,6 +1,27 @@
 import MyOrdersPage from "./MyOrdersPage.jsx";
+import {useDispatch, useSelector} from "react-redux";
+import {useNavigate} from "react-router-dom";
+import {useEffect} from "react";
+import {logout} from "../redux/slices/authSlice.js";
+import {clearCart} from "../redux/slices/cartSlice.js";
 
 const Profile = () => {
+    const { user } = useSelector((state) => state.auth);
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        if (!user) {
+            navigate("/login");
+        }
+    }, [user, navigate]);
+
+    const handleLogout = () => {
+        dispatch(logout());
+        dispatch(clearCart());
+        navigate("/login");
+    }
+
     return (
         <div className={"min-h-screen flex flex-col"}>
             <div className={"flex-grow container mx-auto p-4 md:p-6"}>
@@ -8,12 +29,12 @@ const Profile = () => {
                     {/* left section */}
                     <div className={"w-full md:w-1/3 lg:w-1/4 shadow-md rounded-lg p-6"}>
                         <h1 className={"text-2xl md:text-3xl font-bold mb-4"}>
-                            name
+                            {user?.name}
                         </h1>
                         <p className={"text-lg text-gray-600 mb-4"}>
-                            mail@mail.com
+                            {user?.email}
                         </p>
-                        <button
+                        <button onClick={() => handleLogout()}
                             className={"w-full bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600"}
                         >
                             Logout
